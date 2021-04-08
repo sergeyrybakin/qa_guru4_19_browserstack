0192113ef4477d17a5040b0f2079aff16c0e43fe
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-04-08 22:28:15 +0700

    added Selenide test

---
+++ src/test/java/tests/SelenideAndroidTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import drivers.BrowserstackMobileDriver;
+import io.appium.java_client.MobileBy;
+import org.junit.jupiter.api.Test;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
+
+import static com.codeborne.selenide.Selenide.*;
+import static io.appium.java_client.MobileBy.AccessibilityId;
+
+public class SelenideAndroidTests {
+
+    @Test
+    void searchTest() {
+        Configuration.browser = BrowserstackMobileDriver.class.getName();
+        Configuration.startMaximized = false;
+        Configuration.browserSize = null;
+        Configuration.timeout = 10000;
+
+        open();
+        $(AccessibilityId("Search Wikipedia")).click();
+        $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("BrowserStack");
+        $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_container")).shouldHave(CollectionCondition.sizeGreaterThan(0));
+    }
+
+}
