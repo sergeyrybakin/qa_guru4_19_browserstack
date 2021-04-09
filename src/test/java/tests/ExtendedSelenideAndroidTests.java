145eb60ea1c1285ecbcdd9b37f71de3c10e40f1e
Sergey Rybakin <sergey.rybakin@gmail.com>
2021-04-09 23:16:02 +0700

    added Search of Selenide test

--- src/test/java/tests/ExtendedSelenideAndroidTests.java
+++ src/test/java/tests/ExtendedSelenideAndroidTests.java
@@ -1,6 +1,5 @@
 package tests;
 
-import drivers.BrowserstackMobileDriver;
 import io.appium.java_client.MobileBy;
 
 import org.junit.jupiter.api.DisplayName;
@@ -8,7 +7,7 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import com.codeborne.selenide.CollectionCondition;
-import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Condition;
 
 import static com.codeborne.selenide.Selenide.*;
 import static io.appium.java_client.MobileBy.AccessibilityId;
@@ -30,4 +29,19 @@ public class ExtendedSelenideAndroidTests extends TestBase {
                         .shouldHave(CollectionCondition.sizeGreaterThan(0)));
     }
 
+    @Test
+    @Tag("smoke")
+    @DisplayName("Successful search Selenide in Wiki Android app")
+    void searchSelenideTest() {
+        step("Type search", ()-> {
+            $(AccessibilityId("Search Wikipedia")).click();
+            $(MobileBy.id("org.wikipedia.alpha:id/search_src_text")).setValue("Selenide");
+        });
+        step("Verify suggestion is displayed", ()->
+                $(MobileBy.id("org.wikipedia.alpha:id/search_suggestion"))
+                        .shouldHave(Condition.text("Did you mean \"selenium\"?")));
+        step("Verify content found", ()->
+                $$(MobileBy.id("org.wikipedia.alpha:id/page_list_item_title"))
+                        .get(0).shouldHave(Condition.text("Selenide")));
+    }
 }
